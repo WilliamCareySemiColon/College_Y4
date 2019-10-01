9b94d92c96cfef7da127905d92f4a2746cfa1dc1
WilliamCareySemiColon <wc16315253@gmail.com>
2019-10-01 21:15:17 +0100

    updated documentation and removed redundant work

--- RichWeb/Lab/Week2/problem-1/JS/index.js
+++ RichWeb/Lab/Week2/problem-1/JS/index.js
@@ -1,14 +1,12 @@
-
-var listCollection = [];
-
 var counter = 1;
 
 function AddToList()
 {
+    //get the dom element that contains the text we want to add to the list
     let textfield = document.getElementById("l1-p1-Text");
-
     let textfieldText = textfield.value;
-
+    //ensure there is no white space or null values 
+    //otherwise reset the box and alert the user
     if(
         (textfieldText.length == 0 || 
             textfieldText == null || 
@@ -21,11 +19,11 @@ function AddToList()
         return false;
     }
 
+    //create a list item and attach the word to it
     let listItems = document.createElement("li");
     let wordItem = document.createTextNode(textfieldText);
-
     listItems.appendChild(wordItem);
-
+    //ensure every third item is red
     if(counter % 3 == 0)
     {
         listItems.setAttribute("style","color:red;");
