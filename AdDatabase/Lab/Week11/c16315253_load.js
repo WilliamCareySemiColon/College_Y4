2612382041aa731e707d7a607d55a12e552948a3
WilliamCareySemiColon <wc16315253@gmail.com>
2019-11-30 18:37:32 +0000

    updated db lab work

---
+++ AdDatabase/Lab/Week11/c16315253_load.js
@@ -0,0 +1,161 @@
+//first student
+db.c16315253.insert({
+	Mark: 56,
+	ExamDate: "10/10/2011",
+	Student: {
+		studentid: 1,
+		name: "Mary",
+		surname: "Murray",
+		Nationality: "Irish",
+		Age: 45 },
+	Course: {
+		course_id: "DB",
+		course_name: "Database",
+		credits: 10}
+});
+db.c16315253.insert({
+	Mark: 76,
+	ExamDate: "09/11/2012",
+	Student: {
+		studentid: 1,
+		name: "Mary",
+		surname: "Murray",
+		Nationality: "Irish",
+		Age: 45 },
+	Course: {
+		course_id: "MA",
+		course_name: "Maths",
+		credits: 5}
+});
+db.c16315253.insert({
+	Mark: 45,
+	ExamDate: "02/07/2014",
+	Student: {
+		studentid: 1,
+		name: "Mary",
+		surname: "Murray",
+		Nationality: "Irish",
+		Age: 45 },
+	Course: {
+		course_id: "PR",
+		course_name: "Programming",
+		credits: 15}
+});
+
+//second student
+db.c16315253.insert({
+	Mark: 55,
+	ExamDate: "10/10/2011",
+	Student: {
+		studentid: 2,
+		name: "Bill",
+		surname: "Bellichick",
+		Nationality: "American",
+		Age: 32 },
+	Course: {
+		course_id: "DB",
+		course_name: "Database",
+		credits: 10}
+});
+db.c16315253.insert({
+	Mark: 87,
+	ExamDate: "09/11/2012",
+	Student: {
+		studentid: 2,
+		name: "Bill",
+		surname: "Bellichick",
+		Nationality: "American",
+		Age: 32 },
+	Course: {
+		course_id: "MA",
+		course_name: "Maths",
+		credits: 5}
+});
+db.c16315253.insert({
+	Mark: 45,
+	ExamDate: "10/10/2011",
+	Student: {
+		studentid: 2,
+		name: "Bill",
+		surname: "Bellichick",
+		Nationality: "American",
+		Age: 32 },
+	Course: {
+		course_id: "PR",
+		course_name: "Programming",
+		credits: 15}
+});
+
+//third student
+db.c16315253.insert({
+	Mark: 34,
+	ExamDate: "09/11/2012",
+	Student: {
+		studentid: 3,
+		name: "Tom",
+		surname: "Brady",
+		Nationality: "Canadian",
+		Age: 22 },
+	Course: {
+		course_id: "DB",
+		course_name: "Database",
+		credits: 10}
+});
+db.c16315253.insert({
+	Mark: 56,
+	ExamDate: "02/07/2014",
+	Student: {
+		studentid: 3,
+		name: "Tom",
+		surname: "Brady",
+		Nationality: "Canadian",
+		Age: 22 },
+	Course: {
+		course_id: "MA",
+		course_name: "Maths",
+		credits: 5}
+});
+
+//fourth student
+db.c16315253.insert({
+	Mark: 71,
+	ExamDate: "10/10/2011",
+	Student: {
+		studentid: 4,
+		name: "John ",
+		surname: "Bale",
+		Nationality: "English", 
+		Age: 24},
+	Course: {
+		course_id: "MA",
+		course_name: "Maths",
+		credits: 5}
+});
+db.c16315253.insert({
+	Mark: 87,
+	ExamDate: "09/11/2012",
+	Student: {
+		studentid: 4,
+		name: "John ",
+		surname: "Bale",
+		Nationality: "English", 
+		Age: 24},
+	Course: {
+		course_id: "DB",
+		course_name: "Database",
+		credits: 10}
+});
+db.c16315253.insert({
+	Mark: 95,
+	ExamDate: "09/11/2012",
+	Student: {
+		studentid: 4,
+		name: "John ",
+		surname: "Bale",
+		Nationality: "English", 
+		Age: 24},
+	Course: {
+		course_id: "PR",
+		course_name: "Programming",
+		credits: 15}
+});
