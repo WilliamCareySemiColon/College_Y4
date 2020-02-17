9ff2224eaa8267ac44049009f73366175528b9f4
WilliamCareySemiColon <wc16315253@gmail.com>
2020-02-17 14:18:15 +0000

    worked on the ai lab

--- ArtificalIntelligence/Labs/week4/recursion1.pl
+++ ArtificalIntelligence/Labs/week4/recursion1.pl
@@ -20,3 +20,22 @@ parent_of(petunia,dudley).
 parent_of(vernon,dudley).
 parent_of(lili,harry).
 parent_of(james,harry).
+
+father_of(X,Y) :- parent(X,Y) , male(X), different(X,Y).
+mother_of(X,Y) :- parent(X,Y), female(X), different(X,Y).
+
+grandfather_of(X,Y) :- father_of(X,Z),father_of(Z,Y),different(X,Y).
+
+grandmother_of(X,Y) :- mother_of(X,Z),mother_of(Z,Y),different(X,Y).
+
+sister_of(X,Y) :- (
+	(father_of(Z,X), father_of(Z,Y)) ; (mother_of(Z,X), mother_of(Z,Y))
+	), different(X,Y), female(X).
+	
+brother_of(X,Y) :- (
+	(father_of(Z,X), father_of(Z,Y)) ; (mother_of(Z,X), mother_of(Z,Y))
+	), different(X,Y), male(X).
+	
+aunt_of(X,Y) :- sister_of(X,Z), (father_of(Z,Y) ; mother_of(Z,Y)),different(X,Y).
+
+uncle_of(X,Y) :- brother_of(X,Z), (father_of(Z,Y) ; mother_of(Z,Y)),different(X,Y).
